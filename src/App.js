5ba142c38e392aeab2c247f2d7de6bd2c11d52e6
naval kumar <nawalkumar4810167@gmail.com>
2024-11-11 22:10:57 +0530

    add basic structure code

--- src/App.js
+++ src/App.js
@@ -1,74 +1,29 @@
 import './App.css';
-import Header from "./MyComponents/Header";
-import { Todos } from "./MyComponents/Todos";
-import { Footer } from "./MyComponents/Footer";
-import { AddTodo } from "./MyComponents/AddTodo";
-import { About } from "./MyComponents/About";
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import React from 'react';
+import Homepage from './components/Homepage';
+import './index.css';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
+import ImageGrid from './components/ImageGrid';
 
 function App() {
-  let initTodo;
-  if (localStorage.getItem("todos") === null) {
-    initTodo = [];
-  }
-  else {
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
-
-
-  const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo);
-
-    setTodos(todos.filter((e) => {
-      return e !== todo;
-    }));
-    console.log("deleted", todos)
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
-
-  const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc)
-    let sno;
-    if (todos.length === 0) {
-      sno = 0;
-    }
-    else {
-      sno = todos[todos.length - 1].sno + 1;
-    }
-    const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc,
-    }
-    setTodos([...todos, myTodo]);
-    console.log(myTodo);
-  }
-
-  const [todos, setTodos] = useState(initTodo);
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos])
-
-  return ( 
-    <> 
-    <Router>
-      <Header title="My Todos List" searchBar={false} /> 
-      <Routes>
-          <Route exact path="/" element={
-            <>
-              <AddTodo addTodo={addTodo} />
-              <Todos todos={todos} onDelete={onDelete} /> 
-            </>
-          } /> 
-          <Route exact path="/about" element={<About />} />
-      </Routes> 
-      <Footer />
-    </Router>
-    </>
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <SearchBar />
+      <ImageGrid />
+    </div>
   );
+}
 
+
+function App1() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
